a1e59b0cdda8f19478a03d982b9958de9aa77713
Chen <rchen@CompanyNet.org>
2019-08-09 14:26:25 -0700

    attempt 3

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
     var result = 'App is running'
