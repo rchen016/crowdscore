7f5a9dd2bbb7034eb05ecd9b71e054b33b87f8ad
Chen <rchen@CompanyNet.org>
2019-08-09 14:38:42 -0700

    fixed package json

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
 });
