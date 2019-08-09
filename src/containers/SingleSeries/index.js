12dc2f043fef8eff517c7b5eeb795f05a490f57d
Chen <rchen@CompanyNet.org>
2019-08-09 15:39:06 -0700

    working demo of IMDB rating

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -4,18 +4,32 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 class SingleSeries extends Component{
 	state = {
-		show: null
+		show: null,
+		imdbRating: null
 	}
 
 	componentDidMount(){
 		const { id } = this.props.match.params;
 		fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
 			.then( (response) => response.json() )
-			.then( json => this.setState( { show: json }));
+			.then(
+					json => {
+					this.setState( { show: json });
+					fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e00e4c89&t=${this.state.show.name}`)
+					.then((response) => response.json())
+					.then(
+						json =>{
+							console.log(json.imdbRating);
+							this.setState( { imdbRating: json.imdbRating });
+						}
+					);
+
+				})
 	}
 
 	render(){
 		const { show } = this.state;
+		const { imdbRating } = this.state;
 
 		return(
 			<React.Fragment>
@@ -35,6 +49,9 @@ class SingleSeries extends Component{
 								    	</Card.Text>
 										<Card.Text>
 									  		Episodes - {show._embedded.episodes.length}
+								    	</Card.Text>
+										<Card.Text>
+									  		IMDB - {imdbRating}
 								    	</Card.Text>
 								    	<Button variant="primary">Go somewhere</Button>
 							  		</Card.Body>
