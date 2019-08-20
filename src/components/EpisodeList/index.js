f6d8047e592060b6503acf2ba3eaad88ccb703aa
Chen <rchen@CompanyNet.org>
2019-08-20 15:15:38 -0700

    Episode listed in new line

--- src/components/EpisodeList/index.js
+++ src/components/EpisodeList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./index.css";
 import { Accordion, Card, Button } from 'react-bootstrap'
 
 function getEpisodeNames( mainList, store ){
@@ -7,7 +8,7 @@ function getEpisodeNames( mainList, store ){
 		var obj = mainList[key];
 		for (var prop in obj){
 			if(!obj.hasOwnProperty(prop)) continue;
-			store.push(obj[prop].name);
+			store.push(<li key={prop}>{obj[prop].name}</li>);
 		}
 	}
 }
@@ -16,17 +17,27 @@ const EpisodeList = (props) => {
 	let epiList = ((props.blah || [])._embedded || []);
 	let epiListNames = [];
 	getEpisodeNames(epiList,epiListNames);
-	
+
 	return(
-		<Accordion defaultActiveKey="0">
+		<Accordion>
 			<Card>
 				<Card.Header>
-					<Accordion.Toggle as={Button} variant="link" eventKey="1">
+					<Accordion.Toggle as={Button} variant="link" eventKey="0">
 					Episode List
 					</Accordion.Toggle>
 				</Card.Header>
+				<Accordion.Collapse eventKey="0">
+					<Card.Body>{epiListNames}<br/></Card.Body>
+				</Accordion.Collapse>
+				</Card>
+				<Card>
+				<Card.Header>
+					<Accordion.Toggle as={Button} variant="link" eventKey="1">
+					Click me!
+					</Accordion.Toggle>
+				</Card.Header>
 				<Accordion.Collapse eventKey="1">
-				<Card.Body>{epiListNames}</Card.Body>
+					<Card.Body>Hello! I'm another body</Card.Body>
 				</Accordion.Collapse>
 			</Card>
 		</Accordion>
