3b5f8039c59629d90341eb0f9ce07ec3523d9dea
Chen <rchen@CompanyNet.org>
2019-09-18 13:37:55 -0700

    working register/login through postman

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://crowdscore:zx1230321@cluster0-nvbe5.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 // server.js
-
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const db = require("./config/keys").mongoURI;
+const port = process.env.PORT || 5000;
+const passport = require("passport");
+const users = require("./src/routes/api/users");
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log('Your node js server is running');
+var url = process.env.DATABASEURL || "mongodb://localhost/movie_app";
+mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true});
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use(users);
+
+app.post("./register", function(res,req){
+	console.log("test");
 });
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true, useUnifiedTopology: true }
+//   )
+//   .then(() => console.log("MongoDB successfully connected"))
+//   .catch(err => console.log(err));
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
