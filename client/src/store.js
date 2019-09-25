043bb2a2a9a318c706025e88cba7dad9c4c59def
rickychen10@gmail.com <rickychen10@gmail.com>
2019-09-25 11:48:59 -0700

    removed dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -11,17 +11,17 @@ const middleware = [thunk];
 //     ? applyMiddleware(...middleware)
 //     :  composeWithDevTools(applyMiddleware(...middleware));
 //
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   applyMiddleware(...middleware)
-// );
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(...middleware)
 );
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 export default store;
