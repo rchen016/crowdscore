40450cfc4c6e554fcbb1e13173aff0c2a895df1b
Ricky Chen <ricky.chen@flashtalking.com>
2020-04-27 10:20:35 -0700

    test comment 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 // const db = require("./config/keys").process.env.DATABASEURL;
-//random comment
+//random comment 2
 const passport = require("passport");
 const users = require("./routes/api/users");
 app.use(
